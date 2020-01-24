ec2f71f2326add9b6d1aa8e045cb143824d6e978
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-01-24 15:42:53 +1100

    Changes

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const fileType = require('file-type');
+const bluebird = require('bluebird');
+const multiparty = require('multiparty');
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
+
+// configure the keys for accessing AWS
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+// configure AWS to work with promises
+AWS.config.setPromisesDependency(bluebird);
+
+// create S3 instance
+const s3 = new AWS.S3();
+
+// abstracts function to upload a file returning a promise
+const uploadFile = (buffer, name, type) => {
+  const params = {
+    ACL: 'public-read',
+    Body: buffer,
+    Bucket: process.env.S3_BUCKET,
+    ContentType: type.mime,
+    Key: `${name}.${type.ext}`
+  };
+  return s3.upload(params).promise();
+};
+
+// Define POST route
+app.post('/test-upload', (request, response) => {
+  console.log(request)
+  const form = new multiparty.Form();
+    form.parse(request, async (error, fields, files) => {
+      if (error) throw new Error(error);
+      try {
+        const path = files.file[0].path;
+        const buffer = fs.readFileSync(path);
+        
+        const timestamp = Date.now().toString();
+        const fileName = `bucketFolder/${timestamp}-lg`;
+        const data = await uploadFile(buffer, fileName, {ext:'JPG'});
+        return response.status(200).send(data);
+      } catch (error) {
+        console.log(error)
+        return response.status(400).send(error);
+      }
+    });
+});
+
+const PORT = process.env.PORT || 9000
+
+app.listen(PORT);
+console.log(`Server up and running on port${PORT}...`);
