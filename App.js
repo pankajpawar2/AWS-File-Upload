84c6a9be3bddf8c4ce1973db9e899e9f51b2482e
Pankaj Pawar <54259659+pankajpawar2@users.noreply.github.com>
2020-01-24 15:46:07 +1100

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
